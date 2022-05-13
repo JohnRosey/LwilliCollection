2279c3467243b593f10b1c479acb7659f6cc06ab
Gansonre Ismael <proismaelgans2017@gmail.com>
2022-05-13 17:19:54 -0400

    update

---
+++ js/Api/collection1.js
@@ -0,0 +1,33 @@
+const collection1 = [
+    {
+        id:1,
+        couleur:'noir',
+        sexe:'Femme',
+        collection:'2',
+        description:'Bracelet en cuir Noir Femme',
+        caracteristique:['Bracelet en cuir Femme'],
+        prix:35000,
+        image:['img/revolution/black.jpeg'],
+    },
+    {
+        id:2,
+        couleur:'Rose',
+        sexe:'Femme',
+        collection:'2',
+        description:'Bracelet en cuir Rose  Femme',
+        caracteristique:['Bracelet en cuir Femme'],
+        prix:35000,
+        image:'img/revolution/bene.jpeg',
+    },
+   
+    {
+        id:3,
+        couleur:'Blanc',
+        sexe:'Femme',
+        collection:'2',
+        description:'Bracelet en cuir Blanc Femme',
+        caracteristique:['Bracelet en cuir '],
+        prix:35000,
+        image:'img/revolution/white_girl.jpg',
+    },
+   ]
